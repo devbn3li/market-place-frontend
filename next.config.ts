039e65307b65394f1402305f7b8d493048abec57
devbn3li <muhammed.ali.hissein@gmail.com>
2025-12-29 19:37:13 +0200

    feat: Add global error page and configure remote image patterns in Next.js.

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,25 @@ const nextConfig: NextConfig = {
     // your project has ESLint errors from deprecated options in eslint-config-next
     ignoreDuringBuilds: false,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "www.arabnews.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
